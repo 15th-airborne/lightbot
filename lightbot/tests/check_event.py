099a66efcfbe96dc69f0363f68775a4a1d33eefc
light2077 <435786117@qq.com>
2022-02-22 16:09:32 +0800

    删除冗余

--- lightbot/tests/check_event.py
+++ lightbot/tests/check_event.py
@@ -1,15 +1,15 @@
-import asyncio
-import aiohttp
+# import asyncio
+# import aiohttp
 
 
-async def main():
-    async with aiohttp.ClientSession() as session:
-        async with session.ws_connect("ws://127.0.0.1:6700") as ws_connection:
-            print('连接成功！')
-            while True:
-                event = await ws_connection.receive_json()
-                print(event)
+# async def main():
+#     async with aiohttp.ClientSession() as session:
+#         async with session.ws_connect("ws://127.0.0.1:6700") as ws_connection:
+#             print('连接成功！')
+#             while True:
+#                 event = await ws_connection.receive_json()
+#                 print(event)
 
 
-if __name__ == '__main__':
-    asyncio.run(main())
+# if __name__ == '__main__':
+#     asyncio.run(main())
--- lightbot/tests/tmp.py
+++
@@ -1,5 +0,0 @@
-from plugins.battle.models import get_player
-
-
-p = get_player('2147483647', '565657000')
-print(p)
